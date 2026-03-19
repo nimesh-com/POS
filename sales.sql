f5318a064e2b5b395afedf2243e42080ec824729
Nimesh <user.nimesh@gmail.com>
2026-03-19 17:30:36 +0530

    include Update DB

--- sales.sql
+++ sales.sql
@@ -1,20 +1,21 @@
 -- phpMyAdmin SQL Dump
--- version 3.4.5
--- http://www.phpmyadmin.net
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jan 10, 2015 at 05:39 AM
--- Server version: 5.5.16
--- PHP Version: 5.3.8
+-- Host: 127.0.0.1
+-- Generation Time: Mar 19, 2026 at 12:57 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `sales`
@@ -26,16 +27,15 @@ SET time_zone = "+00:00";
 -- Table structure for table `collection`
 --
 
-CREATE TABLE IF NOT EXISTS `collection` (
-  `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `collection` (
+  `transaction_id` int(11) NOT NULL,
   `date` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `invoice` varchar(100) NOT NULL,
   `amount` varchar(100) NOT NULL,
   `remarks` varchar(100) NOT NULL,
-  `balance` int(11) NOT NULL,
-  PRIMARY KEY (`transaction_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+  `balance` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -43,17 +43,16 @@ CREATE TABLE IF NOT EXISTS `collection` (
 -- Table structure for table `customer`
 --
 
-CREATE TABLE IF NOT EXISTS `customer` (
-  `customer_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `customer` (
+  `customer_id` int(11) NOT NULL,
   `customer_name` varchar(100) NOT NULL,
   `address` varchar(100) NOT NULL,
   `contact` varchar(100) NOT NULL,
   `membership_number` varchar(100) NOT NULL,
   `prod_name` varchar(550) NOT NULL,
   `expected_date` varchar(500) NOT NULL,
-  `note` varchar(500) NOT NULL,
-  PRIMARY KEY (`customer_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+  `note` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -61,8 +60,8 @@ CREATE TABLE IF NOT EXISTS `customer` (
 -- Table structure for table `products`
 --
 
-CREATE TABLE IF NOT EXISTS `products` (
-  `product_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `products` (
+  `product_id` int(11) NOT NULL,
   `product_code` varchar(200) NOT NULL,
   `gen_name` varchar(200) NOT NULL,
   `product_name` varchar(200) NOT NULL,
@@ -74,10 +73,15 @@ CREATE TABLE IF NOT EXISTS `products` (
   `onhand_qty` int(10) NOT NULL,
   `qty` int(10) NOT NULL,
   `qty_sold` int(10) NOT NULL,
-  `expiry_date` varchar(500) NOT NULL,
-  `date_arrival` varchar(500) NOT NULL,
-  PRIMARY KEY (`product_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=58 ;
+  `date_arrival` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `product_code`, `gen_name`, `product_name`, `cost`, `o_price`, `price`, `profit`, `supplier`, `onhand_qty`, `qty`, `qty_sold`, `date_arrival`) VALUES
+(2, 'CFL Bulb', 'Bulb', '     ', '', '500', '750', '250', '', 0, 10, 100, '2026-03-20');
 
 -- --------------------------------------------------------
 
@@ -85,14 +89,13 @@ CREATE TABLE IF NOT EXISTS `products` (
 -- Table structure for table `purchases`
 --
 
-CREATE TABLE IF NOT EXISTS `purchases` (
-  `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `purchases` (
+  `transaction_id` int(11) NOT NULL,
   `invoice_number` varchar(100) NOT NULL,
   `date` varchar(100) NOT NULL,
   `suplier` varchar(100) NOT NULL,
-  `remarks` varchar(100) NOT NULL,
-  PRIMARY KEY (`transaction_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
+  `remarks` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -100,14 +103,13 @@ CREATE TABLE IF NOT EXISTS `purchases` (
 -- Table structure for table `purchases_item`
 --
 
-CREATE TABLE IF NOT EXISTS `purchases_item` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `purchases_item` (
+  `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
   `qty` int(11) NOT NULL,
   `cost` varchar(100) NOT NULL,
-  `invoice` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+  `invoice` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -115,8 +117,8 @@ CREATE TABLE IF NOT EXISTS `purchases_item` (
 -- Table structure for table `sales`
 --
 
-CREATE TABLE IF NOT EXISTS `sales` (
-  `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `sales` (
+  `transaction_id` int(11) NOT NULL,
   `invoice_number` varchar(100) NOT NULL,
   `cashier` varchar(100) NOT NULL,
   `date` varchar(100) NOT NULL,
@@ -125,9 +127,16 @@ CREATE TABLE IF NOT EXISTS `sales` (
   `profit` varchar(100) NOT NULL,
   `due_date` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `balance` varchar(100) NOT NULL,
-  PRIMARY KEY (`transaction_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=142 ;
+  `balance` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`transaction_id`, `invoice_number`, `cashier`, `date`, `type`, `amount`, `profit`, `due_date`, `name`, `balance`) VALUES
+(1, 'RS-25323262', 'Admin', '03/19/26', 'cash', '152250', '50750', '20000', 'bsd', ''),
+(2, 'RS-9036332', 'Admin', '03/19/26', 'cash', '74250', '24750', '2000', '', '');
 
 -- --------------------------------------------------------
 
@@ -135,8 +144,8 @@ CREATE TABLE IF NOT EXISTS `sales` (
 -- Table structure for table `sales_order`
 --
 
-CREATE TABLE IF NOT EXISTS `sales_order` (
-  `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `sales_order` (
+  `transaction_id` int(11) NOT NULL,
   `invoice` varchar(100) NOT NULL,
   `product` varchar(100) NOT NULL,
   `qty` varchar(100) NOT NULL,
@@ -147,9 +156,17 @@ CREATE TABLE IF NOT EXISTS `sales_order` (
   `name` varchar(200) NOT NULL,
   `price` varchar(100) NOT NULL,
   `discount` varchar(100) NOT NULL,
-  `date` varchar(500) NOT NULL,
-  PRIMARY KEY (`transaction_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=315 ;
+  `date` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `sales_order`
+--
+
+INSERT INTO `sales_order` (`transaction_id`, `invoice`, `product`, `qty`, `amount`, `profit`, `product_code`, `gen_name`, `name`, `price`, `discount`, `date`) VALUES
+(1, 'RS-25323262', '1', '3', '2250', '750', 'CFL Bulb', 'Bulb', ' ', '750', '', '03/19/26'),
+(2, 'RS-25323262', '1', '200', '150000', '50000', 'CFL Bulb', 'Bulb', ' ', '750', '', '03/19/26'),
+(3, 'RS-9036332', '2', '99', '74250', '24750', 'CFL Bulb', 'Bulb', ' ', '750', '', '03/19/26');
 
 -- --------------------------------------------------------
 
@@ -157,15 +174,14 @@ CREATE TABLE IF NOT EXISTS `sales_order` (
 -- Table structure for table `supliers`
 --
 
-CREATE TABLE IF NOT EXISTS `supliers` (
-  `suplier_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `supliers` (
+  `suplier_id` int(11) NOT NULL,
   `suplier_name` varchar(100) NOT NULL,
   `suplier_address` varchar(100) NOT NULL,
   `suplier_contact` varchar(100) NOT NULL,
   `contact_person` varchar(100) NOT NULL,
-  `note` varchar(500) NOT NULL,
-  PRIMARY KEY (`suplier_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `note` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -173,14 +189,13 @@ CREATE TABLE IF NOT EXISTS `supliers` (
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `position` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+  `position` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data for table `user`
@@ -190,6 +205,123 @@ INSERT INTO `user` (`id`, `username`, `password`, `name`, `position`) VALUES
 (1, 'admin', 'admin', 'Admin', 'admin'),
 (2, 'cashier', 'cashier', 'Cashier Pharmacy', 'Cashier'),
 (3, 'admin', 'admin123', 'Administrator', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `collection`
+--
+ALTER TABLE `collection`
+  ADD PRIMARY KEY (`transaction_id`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`product_id`);
+
+--
+-- Indexes for table `purchases`
+--
+ALTER TABLE `purchases`
+  ADD PRIMARY KEY (`transaction_id`);
+
+--
+-- Indexes for table `purchases_item`
+--
+ALTER TABLE `purchases_item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`transaction_id`);
+
+--
+-- Indexes for table `sales_order`
+--
+ALTER TABLE `sales_order`
+  ADD PRIMARY KEY (`transaction_id`);
+
+--
+-- Indexes for table `supliers`
+--
+ALTER TABLE `supliers`
+  ADD PRIMARY KEY (`suplier_id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `collection`
+--
+ALTER TABLE `collection`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `purchases`
+--
+ALTER TABLE `purchases`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `purchases_item`
+--
+ALTER TABLE `purchases_item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `sales_order`
+--
+ALTER TABLE `sales_order`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `supliers`
+--
+ALTER TABLE `supliers`
+  MODIFY `suplier_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
